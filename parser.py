42909f02b500f9aa7e8ff3bdff8fc6fce5fe1604
Arne Küderle <a.kuederle@gmail.com>
2016-12-19 11:05:29 +0100

    refactoring

--- parser.py
+++ parser.py
@@ -1,8 +1,5 @@
-import re
-import os
-import shutil
-
 import argparse
+import utils
 
 
 parser = argparse.ArgumentParser(description='Render a LaTex Template with variables.')
@@ -16,50 +13,12 @@ project = "./"
 in_file = args['in']
 build_d = "{}.build/".format(project)
 template_file = args['template']
-out_file = "{}renderer_template".format(build_d)
+out_file = "{}renderer_template.pdf".format(project)
 
+options = utils.get_options_from_file(in_file)
 
-current_object = None
-current_content = ""
-latex_formated_variables = ""
+template = utils.get_template(template_file)
 
-with open(in_file) as f:
-    content = f.read()
-    keys = re.findall(r"%(.+):", content)
-    values = re. findall(r":\s*([\w\W]+?)\s*(?:%|$)", content)
-
-options = dict(zip(keys, values))
-
-######
-# modified from http://eosrei.net/articles/2015/11/latex-templates-python-and-jinja2-generate-pdfs
-import jinja2
-import os
-from jinja2 import Template
-latex_jinja_env = jinja2.Environment(
-	block_start_string = '\BLOCK{',
-	block_end_string = '}',
-	variable_start_string = '\VAR{',
-	variable_end_string = '}',
-	comment_start_string = '\#{',
-	comment_end_string = '}',
-	line_statement_prefix = '%%',
-	line_comment_prefix = '%#',
-	trim_blocks = True,
-	autoescape = False,
-	loader = jinja2.FileSystemLoader(os.path.abspath('/'))
-)
-template = latex_jinja_env.get_template(os.path.realpath(template_file))
-######
+utils.compile_pdf_from_template(template, options, out_file)
 
 renderer_template = template.render(**options)
-
-if not os.path.exists(build_d):  # create the build directory if not exisiting
-    os.makedirs(build_d)
-
-with open(out_file+".tex", "w") as f:  # saves tex_code to outpout file
-    f.write(renderer_template)
-
-
-os.system('pdflatex -output-directory {} {}'.format(os.path.realpath(build_d), os.path.realpath(out_file + '.tex')))
-
-shutil.copy2(out_file+".pdf", os.path.dirname(os.path.realpath(in_file)))
